1e7ab5d77449a43bb3370872508cda3614a4fd34
root <root@ip-172-31-46-139.ap-northeast-2.compute.internal>
2021-01-14 06:49:06 +0000

    ADD private mediafiles settings

--- app/upload/models.py
+++ app/upload/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from config.storage_backends import PublicMediaStorage, PrivateMediaStorage
+
 # Create your models here.
+
+class Upload(models.Model):
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(storage=PublicMediaStorage())
+
+class Upload(models.Model):
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(storage=PrivateMediaStorage())
--- app/upload/views.py
+++ app/upload/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
+from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
+from .models import Upload
 
 def image_upload(request):
     if request.method == "POST" and request.FILES["image_file"]:
         image_file = request.FILES["image_file"]
-        fs = FileSystemStorage()
-        filename = fs.save(image_file.name, image_file)
-        image_url = fs.url(filename)
-        print(image_url)
+        image_type = request.POST['image_type']
+        if settings.USE_S3:
+            if image_type == 'private':
+                upload = UploadPrivate(file=image_file)
+            else:
+                upload = Upload(file=image_file)
+            upload.save()
+            image_url = upload.file.url
+        else:
+            fs = FileSystemStorage()
+            filename = fs.save(image_file.name, image_file)
+            image_url = fs.url(filename)
         return render(request, "upload.html", {
             "image_url": image_url
         })
